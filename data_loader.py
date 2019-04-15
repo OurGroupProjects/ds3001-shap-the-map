83bd971a787565a73100fcf6033ef158abe75d97
Isaac Woods <isaac.woods.official@gmail.com>
2019-04-15 14:23:38 -0400

    Implement API key loading from secrets.json

---
+++ data_loader.py
@@ -0,0 +1,11 @@
+import json
+from pprint import pprint
+
+# API key should be loaded from your secrets.json file
+PLACES_API_KEY: ''
+
+# Load the api key from secrets.json
+with open('secrets.json') as secrets:
+    data = json.load(secrets)
+    PLACES_API_KEY = data['api_keys']['google_places']
+
