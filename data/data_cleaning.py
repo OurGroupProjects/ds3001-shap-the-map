d74f32371a229e05346b1126d8ceebb293190201
Isaac Woods <isaac.woods.official@gmail.com>
2019-04-18 01:39:08 -0400

    Create datasets to compare fuzzy filter method

--- data/data_cleaning.py
+++ data/data_cleaning.py
@@ -2,13 +2,15 @@ import sys
 import csv
 import json
 from pprint import pprint
-from fuzzywuzzy import process
+from fuzzywuzzy import process, fuzz
 
 # Json file to start with
 json_file = 'business.json'
 
+# Output version
+out_version = '_v2'
 # CSV file to write to
-csv_file = 'business.csv'
+csv_file = 'business'+ out_version +'.csv'
 
 # What parts of the json to keep
 columns = [
@@ -34,10 +36,11 @@ chains = ["McDonalds","Burger King","Wendy's","Chick Fil A","Five Guys","Arby's"
           "Good Times Burgers & Frozen Custard","Roy Rogers","Teddy's Bigger Burgers"
 ]
 
-fuzzy_cutoff = 89
+fuzzy_cutoff = 90
 use_fuzzy = True
 min_len_for_fuzzy = 5
 chains_found = {key: 0 for key in chains}
+chain_fuzzy_matches = {key: set() for key in chains}
 
 # Retrieve relevent attributes from json entry
 def parse_entry(entry):
@@ -61,9 +64,10 @@ def filter_entry(entry):
         chains_found[name]+=1
         return True
     elif use_fuzzy and len(name) >= min_len_for_fuzzy:
-        chain_name, ratio = process.extractOne(name, chains)
+        chain_name, ratio = process.extractOne(name, chains, scorer=fuzz.partial_ratio)
         if ratio > fuzzy_cutoff and len(chain_name) >= min_len_for_fuzzy:
             # print("Similarity of {} between {} and {}".format(ratio, name, chain_name))
+            chain_fuzzy_matches[chain_name].add(name)
             chains_found[chain_name]+=1
             return True
     return False
@@ -92,4 +96,5 @@ with open(csv_file, 'w+', encoding='UTF8', newline='') as fout:
                 row = parse_entry(entry)
                 csv_out.writerow(row)
 
-pprint(chains_found)
+pprint(chains_found, open('chains_found' + out_version + '.txt', 'w'))
+pprint(chain_fuzzy_matches, open('chain_fuzzy_matches' + out_version + '.txt', 'w'))
