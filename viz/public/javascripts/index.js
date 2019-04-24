ea1954c85c8d2b8150adfcc1f400db30e36bc0b8
karstenroberts <karstenroberts@gmail.com>
2019-04-24 18:24:14 -0400

    Refactor index.js

--- viz/public/javascripts/index.js
+++ viz/public/javascripts/index.js
@@ -1,3 +1,8 @@
+let geojson;
+var map = L.map('map').setView([37.8, -96], 4);
+var info = L.control();
+
+
 let dataReq = new XMLHttpRequest();
 dataReq.addEventListener("load", onDataLoad);
 dataReq.open("GET", "/data");
@@ -5,7 +10,6 @@ dataReq.send();
 
 function onDataLoad() {
   const stateData = JSON.parse(this.responseText);
-  var map = L.map('map').setView([37.8, -96], 4);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGd3aWxlcyIsImEiOiJjanV2bzhvdTEwM3NnNGRwYmIzd3Ixd3h5In0.jFIY4jpuTwWO4F_Pvbz31w', {
     maxZoom: 18,
@@ -17,7 +21,6 @@ function onDataLoad() {
 
 
 // control that shows state info on hover
-  var info = L.control();
 
   info.onAdd = function (map) {
     this._div = L.DomUtil.create('div', 'info');
@@ -33,66 +36,6 @@ function onDataLoad() {
 
   info.addTo(map);
 
-
-// get color depending on population density value
-  function getColor(d) {
-    return d > 1000 ? '#0c2c84' :
-        d > 500 ? '#225ea8' :
-            d > 200 ? '#1d91c0' :
-                d > 100 ? '#41b6c4' :
-                    d > 50 ? '#7fcdbb' :
-                        d > 20 ? '#c7e9b4' :
-                            d > 10 ? '#edf8b1' :
-                                '#ffffd9';
-  }
-
-  function style(feature) {
-    return {
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      fillOpacity: 0.7,
-      fillColor: getColor(feature.properties.density)
-    };
-  }
-
-  function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-      weight: 5,
-      color: '#666',
-      dashArray: '',
-      fillOpacity: 0.7
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-      layer.bringToFront();
-    }
-
-    info.update(layer.feature.properties);
-  }
-
-  var geojson;
-
-  function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
-  }
-
-  function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-  }
-
-  function onEachFeature(feature, layer) {
-    layer.on({
-      mouseover: highlightFeature,
-      mouseout: resetHighlight,
-      click: zoomToFeature
-    });
-  }
-
   document.body.onkeyup = function (e) {
     if (e.key === " ") {
       map.setView([37.8, -96], 4);
@@ -103,31 +46,81 @@ function onDataLoad() {
     style: style,
     onEachFeature: onEachFeature
   }).addTo(map);
-
   map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
-
 
   var legend = L.control({position: 'bottomleft'});
 
   legend.onAdd = function (map) {
-
     var div = L.DomUtil.create('div', 'info legend'),
         grades = [0, 10, 20, 50, 100, 200, 500, 1000],
         labels = [],
         from, to;
-
     for (var i = 0; i < grades.length; i++) {
       from = grades[i];
       to = grades[i + 1];
-
       labels.push(
           '<i style="background:' + getColor(from + 1) + '"></i> ' +
           from + (to ? '&ndash;' + to : '+'));
     }
-
     div.innerHTML = labels.join('<br>');
     return div;
   };
-
   legend.addTo(map);
+}
+
+// get color depending on population density value
+function getColor(d) {
+  return d > 1000 ? '#0c2c84' :
+      d > 500 ? '#225ea8' :
+          d > 200 ? '#1d91c0' :
+              d > 100 ? '#41b6c4' :
+                  d > 50 ? '#7fcdbb' :
+                      d > 20 ? '#c7e9b4' :
+                          d > 10 ? '#edf8b1' :
+                              '#ffffd9';
+}
+
+function style(feature) {
+  return {
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7,
+    fillColor: getColor(feature.properties.density)
+  };
+}
+
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+
+  info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+  info.update();
+}
+
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature
+  });
 }
